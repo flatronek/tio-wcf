1bf6afdee8fd81c0d070b613711032e8bc09b4cc
SB <sbrandys13@gmail.com>
2016-03-13 20:46:01 +0100

    client added

--- Space/Space/Program.cs
+++ Space/Space/Program.cs
@@ -38,4 +38,24 @@ namespace Space
             }
         }
     }
+
+    public class Starship
+    {
+        public string Name { get; set; }
+        public Person Captain { get; set; }
+        public List<Person> Crew { get; set; }
+
+        public void TestMethod()
+        {
+
+        }
+
+        public void PresentCrew()
+        {
+            Console.WriteLine("Ship: {0}", Name);
+            Console.WriteLine("Captain: {0}, {1}", Captain.Name, Captain.Age);
+
+            Crew.ForEach(p => Console.WriteLine("Crew member: {0}, {1}", p.Name, p.Age));
+        }
+    }
 }
---
+++ Space/SpaceClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using SpaceClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BlackholeClient bc = new BlackholeClient();
+                Console.WriteLine("Client created...");
+
+                Starship ship = CreateStarship(35, 5);
+
+                Console.WriteLine("Ultimae answer: {0} ", bc.UltimateAnswer());
+                bc.PullStarship(ship);
+
+                Console.ReadLine();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine("Error occurred: " + e.Message);
+            }
+        }
+
+        static Starship CreateStarship(int captainAge, int crewSize)
+        {
+            Person captain = new Person() { Name = "captain jack", Age = captainAge };
+
+            List<Person> crew = new List<Person>();
+            for (int i = 0; i < crewSize; i++)
+            {
+                crew.Add(new Person() { Name = "crewMember" + i, Age = captainAge - i });
+            }
+
+            return new Starship() { Name = "ship1", Captain = captain, Crew = crew.ToArray() };
+        }
+    }
+}
